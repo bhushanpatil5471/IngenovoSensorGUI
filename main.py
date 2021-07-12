2974cdda779dda0a16901b7dabbfc3928c5707b2
Bhushan Patil <bhushan.patil@jashds.com>
2021-07-12 12:06:29 +0530

    Main py changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
+import uvicorn
 
 from ControllerEndPoints.ProjectsController import origins
 from Utils import NewAccessToken
@@ -10,6 +11,12 @@ app = FastAPI()
 
 app.include_router(ProjectsController.router)
 app.include_router(LoginController.login)
+
+origins = [
+    "http://localhost:4200",
+    "http://localhost:8080",
+    "*"
+]
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,3 +35,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
